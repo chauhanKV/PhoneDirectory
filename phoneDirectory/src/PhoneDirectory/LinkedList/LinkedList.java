f1e58420c4ea388833650d5eea986a895e58d5de
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2021-11-10 14:42:48 +0530

    Added addFirst() , addLast(), removeLast(), removeFirst(), contains(), indexOf() method implementation for custom linkedlist

--- phoneDirectory/src/PhoneDirectory/LinkedList/LinkedList.java
+++ phoneDirectory/src/PhoneDirectory/LinkedList/LinkedList.java
@@ -1,37 +1,122 @@
 package PhoneDirectory.LinkedList;
 
-public class LinkedList {
-//private Node first;
-//private Node last;
+import java.util.Objects;
 
-public void addFirst()
-{
+public class LinkedList<T> {
+    private class Node<T> {
+        private T value;
+        private Node<T> next;
 
-}
+        public Node(T value) {
+            this.value = value;
+        }
+    }
 
-public void addLast()
-{
+    private Node<T> first;
+    private Node<T> last;
+    private int size = 0;
 
-}
+    // Time Complexity -> O(1)
+    public void addFirst(T element) {
+        var node = new Node(element);
 
-public void deleteFirst()
-{
+        //First check if the list is empty
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            node.next = first;
+            first = node;
+        }
+        size++;
+    }
 
-}
+    //Time Complexity -> O(1)
+    public void addLast(T element) {
+        var node = new Node(element);
 
-public void deleteLast()
-{
+        // First check if the list is empty
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            //append the node to last node's next and update the last node
+            last.next = node;
+            last = node;
+        }
+        size++;
+    }
 
-}
+    private boolean isEmpty() {
+        return first == null;
+    }
 
-public void contains()
-{
+    // Time complexity -> O(1)
+    public void removeFirst() {
+        if(isEmpty())
+            throw new IndexOutOfBoundsException();
 
-}
+      var second = first.next;
+      first.next = null;
+      first = second;
+      size--;
+    }
 
-public void indexOf()
-{
+    // Time Complexity -> O(N)
+    public void removeLast() {
+        if (isEmpty())
+            throw new IndexOutOfBoundsException();
 
-}
+        var current = first;
+        while (current.next != last) {
+            current = current.next;
+        }
+        last = current;
+        last.next = null;
+        size--;
+    }
+
+    // Time Complexity -> O(N)
+    public boolean contains(T element) {
+        if (isEmpty()) return false;
+        var current = first;
+
+        while (current != last) {
+            if (current.value == element) {
+                return true;
+            } else {
+                current = current.next;
+            }
+        }
+        return false;
+    }
+
+    //Time Complexity -> O(N)
+    public int indexOf(T element) {
+        if(isEmpty()) return -1;
+        var current = first;
+        var counter = 0;
+        while (current != last) {
+            if (current.value == element)
+                return counter;
+            else
+            {
+                current = current.next;
+                counter++;
+            }
+        }
+        return -1;
+    }
+
+    private int size()
+    {
+        return size;
+    }
+
+//    // Need to create generic array -> refer ->  https://www.baeldung.com/java-generic-array
+//    public T[] printList() {
+//        T[] array = new T[size];
+//
+//
+//        return array;
+//    }
 
 }
